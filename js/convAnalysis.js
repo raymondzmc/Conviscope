15c578243db9c42b78d31a475e6040e44b7dcf75
Raymond Li <li.raymond96@gmail.com>
2020-07-23 12:25:00 -0700

    added topic view

--- js/convAnalysis.js
+++ js/convAnalysis.js
@@ -11,7 +11,8 @@ class ConvAnalysis {
 
     // Number of conversations in the focused view
     // TODO: Could be a passed-in parameter
-    this.numFocused = 50;
+    this.numFocused = _config.numFocused;
+    this.numTopics = _config.numTopics;
 
     // Maps line polarity to color bins
     this.sentimentBin = val => {
@@ -35,9 +36,12 @@ class ConvAnalysis {
     let vis = this;
     vis.config.innerHeight = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
     vis.config.innerWidth = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
-    vis.config.focusHeight = 0.7 * vis.config.innerHeight;
-    vis.config.contextHeight = 0.15 * vis.config.innerHeight;
-    vis.config.barWidth = vis.config.containerWidth / 4;
+    vis.config.focusWidth = 0.7 * vis.config.innerWidth;
+    vis.config.contextWidth = 0.15 * vis.config.innerWidth;
+    vis.config.barHeight = vis.config.containerHeight / 4;
+
+    // Define height s.t. each grid is a square
+    vis.config.heatMapHeight = (vis.config.focusWidth / vis.numFocused) * vis.numTopics;
     vis.svgContainer = d3.select(vis.config.parentElement).append("svg")
       .attr("height", vis.config.containerHeight)
       .attr("width", vis.config.containerWidth);
@@ -46,191 +50,382 @@ class ConvAnalysis {
   updateVis() {
     let vis = this;
 
-    vis.numConv = vis.focusData.length;
+    vis.numConv = vis.data.length;
 
     // Use the latest 50 conversations for current view
     vis.focusIndex = {'start': vis.numConv - vis.numFocused, 'end': vis.numConv};
     vis.focusContainer = vis.svgContainer.append('g');
+    vis.topicFocusContainer = vis.svgContainer.append('g');
     vis.contextContainer = {
-      'top': vis.svgContainer.append('g'),
-      'bottom': vis.svgContainer.append('g')
+      'left': vis.svgContainer.append('g'),
+      'right': vis.svgContainer.append('g')
+    }
+    vis.topicContextContainer = {
+      'left': vis.svgContainer.append('g'),
+      'right': vis.svgContainer.append('g')
     }
     vis.renderContext();
     vis.renderFocus();
+    vis.renderTopicFocus();
     vis.renderSelector();
+    vis.renderTopicContext();
   }
 
 
   renderContext() {
     let vis = this;
-    let beforeData = vis.focusData.slice(0, vis.focusIndex.start);
-    let afterData = vis.focusData.slice(vis.focusIndex.end);
+    let beforeData = vis.data.slice(0, vis.focusIndex.start);
+    let afterData = vis.data.slice(vis.focusIndex.end);
 
-    // The starting height of the top and bottom context
+    // The starting point of the left and right context
     let contextStart = [
-      vis.config.margin.top,
-      vis.config.margin.top + 0.85 * vis.config.innerHeight
+      vis.config.margin.left,
+      vis.config.margin.left + 0.85 * vis.config.innerWidth
     ]
 
-    let contextHeight = vis.config.contextHeight;
+    let contextWidth = vis.config.contextWidth;
 
     let stack = d3.stack()
       .keys([0, 1, 2, 3, 4])
-      .value((d, key) => d.sentWidth[key])
+      .value((d, key) => d.sentHeight[key])
       .offset(d3.stackOffsetExpand); // Normalize between 0 and 1
 
-    let stackTop = stack(beforeData);
-    let stackBottom = stack(afterData);
+    let stackLeft = stack(beforeData);
+    let stackRight = stack(afterData);
 
-    // Define the y-scale for top and bottom context
-    const yScaleTop = d3.scaleBand()
+
+    // Define the x-scale for left and right context
+    const xScaleLeft = d3.scaleBand()
       .domain(beforeData.map(d => d.title))
-      .range([contextStart[0], contextStart[0] + contextHeight]);
+      .range([contextStart[0], contextStart[0] + contextWidth]);
 
-    const yScaleBottom = d3.scaleBand()
+    console.log(`Context Range Left: ${contextStart[0]}, ${contextStart[0] + contextWidth}`)
+
+    const xScaleRight = d3.scaleBand()
       .domain(afterData.map(d => d.title))
-      .range([contextStart[1], contextStart[1] + contextHeight]);
+      .range([contextStart[1], contextStart[1] + contextWidth]);
 
-    const width = vis.config.barWidth;
-    const xScale = d3.scaleLinear()
-      .range([0, width]);
+    console.log(`Context Range Right: ${contextStart[1]}, ${contextStart[1] + contextWidth}`)
 
-    let areaTop = d3.area()
-      .y(d => yScaleTop(d.data.title))
-      .x0(d => xScale(d[0]))
-      .x1(d => xScale(d[1]));
+    const height = vis.config.barHeight;
+    const yScale = d3.scaleLinear()
+      .range([0, height]);
 
-    let areaBottom = d3.area()
-      .y(d => yScaleBottom(d.data.title))
-      .x0(d => xScale(d[0]))
-      .x1(d => xScale(d[1]));
+    let areaLeft = d3.area()
+      .x(d => xScaleLeft(d.data.title))
+      .y0(d => yScale(d[0]))
+      .y1(d => yScale(d[1]));
+
+    let areaRight = d3.area()
+      .x(d => xScaleRight(d.data.title))
+      .y0(d => yScale(d[0]))
+      .y1(d => yScale(d[1]));
 
     let subgroups = ["sent0", "sent1", "sent2", "sent3", "sent4"];
     const colorScale = d3.scaleOrdinal()
       .domain(subgroups)
       .range(['#7d57bd','#aaa3cd','#f0f0f0', '#fdb863', '#e66101'])
 
-    let pathTop = vis.contextContainer.top.selectAll('path').data(stackTop);
-    let pathBottom = vis.contextContainer.bottom.selectAll('path').data(stackBottom);
+    let pathLeft = vis.contextContainer.left.selectAll('path').data(stackLeft);
+    let pathRight = vis.contextContainer.right.selectAll('path').data(stackRight);
 
-    console.log(pathTop);
-    let pathTopEnter = pathTop.enter()
+    let pathLeftEnter = pathLeft.enter()
       .append('path')
-      .merge(pathTop)
+      .merge(pathLeft)
         .attr('fill', d => colorScale(d.key))
-        .attr('d', areaTop);
+        .attr('d', areaLeft);
 
-    let pathBottomEnter = pathBottom.enter()
+    let pathRightEnter = pathRight.enter()
       .append('path')
-      .merge(pathBottom)
+      .merge(pathRight)
         .attr('fill', d => colorScale(d.key))
-        .attr('d', areaBottom);
-      // .join('path')
-      //   .attr('fill', d => colorScale(d.key))
-      //   .attr('d', area);
+        .attr('d', areaRight);
 
   }
 
   renderFocus() {
     let vis = this;
-    let data = vis.focusData.slice(vis.focusIndex.start, vis.focusIndex.end);
-    console.log("rendering focus");
+    let data = vis.data.slice(vis.focusIndex.start, vis.focusIndex.end);
 
     let subgroups = ["sent0", "sent1", "sent2", "sent3", "sent4"];
 
     // Define stack for different sentiments
     let stack = d3.stack()
       .keys([0, 1, 2, 3, 4])
-      .value((d, key) => d.sentWidth[key])
+      .value((d, key) => d.sentHeight[key])
       .order(d3.stackOrderNone);
 
     let stackedData = stack(data);
-
     stackedData = stackedData[0].map((_, colIndex) =>
       stackedData.map((row) => row[colIndex])
     );
 
-    // console.log(stackedData);
-
-    let yRange = [
-      vis.config.margin.top + vis.config.contextHeight,
-      vis.config.margin.top + vis.config.contextHeight + vis.config.focusHeight
+    let xRange = [
+      vis.config.margin.left + vis.config.contextWidth,
+      vis.config.margin.left + vis.config.contextWidth + vis.config.focusWidth
     ]
 
+    console.log(`Focus Range ${xRange[0]}, ${xRange[1]}`)
     // Define scales
-    const yScale = d3.scaleBand()
+    const xScale = d3.scaleBand()
       .domain(data.map(d => d.title))
-      .range(yRange)
+      .range(xRange)
       .padding(0.05);
 
-    const xScale = d3.scaleLinear()
-      .range([0, vis.config.barWidth]);
+    const yScale = d3.scaleLinear()
+      .range([0, vis.config.barHeight]);
 
     const colorScale = d3.scaleOrdinal()
       .domain(subgroups)
       .range(['#7d57bd','#aaa3cd','#f0f0f0', '#fdb863', '#e66101'])
 
     // Draw SVG
-    let rows = vis.focusContainer.selectAll('g').data(stackedData);
-    let rowsEnter = rows.enter()
+    let cols = vis.focusContainer.selectAll('g').data(stackedData);
+    let colsEnter = cols.enter()
       .append("g")
-      .attr('transform', d => `translate(0, ${yScale(d[0].data.title)})`);
+      .attr('transform', d => `translate(${xScale(d[0].data.title)}, 0)`);
 
-    let rect = rowsEnter.merge(rows).selectAll('rect').data(d => d);
+    let rect = colsEnter.merge(cols).selectAll('rect').data(d => d);
     let rectEnter = rect.enter();
 
     rectEnter
       .append('rect')
       .merge(rect)
       .attr("class", "sentiment-bar")
-      .attr("width", d => xScale((d[1] - d[0]) / d.data.sentTotal))
-      .attr("height", yScale.bandwidth())
-      .attr("x", d => xScale(d[0] / d.data.sentTotal))
+      .attr("height", d => yScale((d[1] - d[0]) / d.data.sentTotal))
+      .attr("width", xScale.bandwidth())
+      .attr("y", d => yScale(d[0] / d.data.sentTotal))
       .attr('fill', (d, i) => colorScale(i));
   }
 
+  renderTopicFocus() {
+    let vis = this;
+    let data = vis.data.slice(vis.focusIndex.start, vis.focusIndex.end);
+
+    let xRange = [
+      vis.config.margin.left + vis.config.contextWidth,
+      vis.config.margin.left + vis.config.contextWidth + vis.config.focusWidth
+    ]
+
+    let yRange = [
+      vis.config.barHeight,
+      vis.config.barHeight + vis.config.heatMapHeight
+    ]
+
+    const xScale = d3.scaleBand()
+      .domain(data.map(d => d.title))
+      .range(xRange)
+      .padding(0.05);
+
+    const yScale = d3.scaleBand()
+      .domain(Array.from(Array(vis.numTopics).keys()))
+      .range(yRange)
+      .padding(0.05);
+
+    const colorScale = d3.scaleOrdinal()
+      .domain([0, 1])
+      .range(['#ffffff', '#000000']);
+
+    let cols = vis.topicFocusContainer.selectAll('g').data(data);
+    let colsEnter = cols.enter()
+      .append("g")
+      .attr('transform', d => `translate(${xScale(d.title)}, 0)`);
+
+    let rect = colsEnter.merge(cols).selectAll('rect').data(d => d.topicHeatmap);
+    let rectEnter = rect.enter();
+    rectEnter
+      .append('rect')
+      .merge(rect)
+      .attr("class", "topic-grid")
+      .attr("height", yScale.bandwidth())
+      .attr("width", xScale.bandwidth())
+      .attr("y", (d, i) => yScale(i))
+      .attr('fill', d => colorScale(d));
+  }
+
+  renderTopicContext() {
+    let vis = this;
+    let beforeData = vis.data.slice(0, vis.focusIndex.start);
+    let afterData = vis.data.slice(vis.focusIndex.end);
+
+    let stack = d3.stack()
+      .keys([0, 1, 2, 3, 4])
+      .value((d, key) => d.sentHeight[key])
+      .offset(d3.stackOffsetExpand); // Normalize between 0 and 1
+
+    // The starting point of the left and right context
+    let contextStart = [
+      vis.config.margin.left,
+      vis.config.margin.left + 0.85 * vis.config.innerWidth
+    ]
+
+    let contextWidth = vis.config.contextWidth;
+
+    // let stack = d3.stack()
+    //   .keys([0, 1, 2, 3, 4])
+    //   .value((d, key) => d.sentHeight[key])
+    //   .offset(d3.stackOffsetExpand); // Normalize between 0 and 1
+
+    // let stackLeft = stack(beforeData);
+    // let stackRight = stack(afterData);
+
+    let yRange = [
+      vis.config.barHeight,
+      vis.config.barHeight + vis.config.heatMapHeight
+    ]
+
+    const xScaleLeft = d3.scaleBand()
+      .domain(beforeData.map(d => d.title))
+      .range([contextStart[0], contextStart[0] + contextWidth]);
+
+    const xScaleRight = d3.scaleBand()
+      .domain(afterData.map(d => d.title))
+      .range([contextStart[1], contextStart[1] + contextWidth]);
+
+    const yScale = d3.scaleBand()
+      .domain(Array.from(Array(vis.numTopics).keys()))
+      .range(yRange)
+      .padding(0.05);
+
+    const colorScale = d3.scaleOrdinal()
+      .domain([0, 1])
+      .range(['#ffffff', '#000000']);
+
+    let colsLeft = vis.topicContextContainer.left.selectAll('g').data(beforeData);
+    let colsRight = vis.topicContextContainer.right.selectAll('g').data(afterData);
+    let colsLeftEnter = colsLeft.enter()
+      .append("g");
+    let colsRightEnter = colsRight.enter()
+      .append("g");
+
+    colsLeft.merge(colsLeftEnter)
+      .attr('transform', d => `translate(${xScaleLeft(d.title)}, 0)`);
+    colsRight.merge(colsRightEnter)
+      .attr('transform', d => `translate(${xScaleRight(d.title)}, 0)`);
+
+    let rectLeft = colsLeftEnter.merge(colsLeft).selectAll('rect').data(d => d.topicHeatmap);
+    let rectRight = colsRightEnter.merge(colsRight).selectAll('rect').data(d => d.topicHeatmap);
+
+    let rectLeftEnter = rectLeft.enter();
+    let rectRightEnter = rectRight.enter();
+
+    rectLeftEnter
+      .append('rect')
+      .merge(rectLeft)
+      .attr("height", yScale.bandwidth())
+      .attr("width", xScaleLeft.bandwidth())
+      .attr("y", (d, i) => yScale(i))
+      .attr('fill', d => colorScale(d));
+
+    rectRightEnter
+      .append('rect')
+      .merge(rectRight)
+      .attr("height", yScale.bandwidth())
+      .attr("width", xScaleRight.bandwidth())
+      .attr("y", (d, i) => yScale(i))
+      .attr('fill', d => colorScale(d));
+
+    colsLeft.exit().remove();
+    colsRight.exit().remove();
+    rectLeft.exit().remove();
+    rectRight.exit().remove()
+
+
+
+    // Define the x-scale for left and right context
+    // const xScaleLeft = d3.scaleBand()
+    //   .domain(beforeData.map(d => d.title))
+    //   .range([contextStart[0], contextStart[0] + contextWidth]);
+
+    // console.log(`Context Range Left: ${contextStart[0]}, ${contextStart[0] + contextWidth}`)
+
+    // const xScaleRight = d3.scaleBand()
+    //   .domain(afterData.map(d => d.title))
+    //   .range([contextStart[1], contextStart[1] + contextWidth]);
+
+    // console.log(`Context Range Right: ${contextStart[1]}, ${contextStart[1] + contextWidth}`)
+
+    // const height = vis.config.barHeight;
+    // const yScale = d3.scaleLinear()
+    //   .range([0, height]);
+
+    // let areaLeft = d3.area()
+    //   .x(d => xScaleLeft(d.data.title))
+    //   .y0(d => yScale(d[0]))
+    //   .y1(d => yScale(d[1]));
+
+    // let areaRight = d3.area()
+    //   .x(d => xScaleRight(d.data.title))
+    //   .y0(d => yScale(d[0]))
+    //   .y1(d => yScale(d[1]));
+
+    // let subgroups = ["sent0", "sent1", "sent2", "sent3", "sent4"];
+    // const colorScale = d3.scaleOrdinal()
+    //   .domain(subgroups)
+    //   .range(['#7d57bd','#aaa3cd','#f0f0f0', '#fdb863', '#e66101'])
+
+    // let pathLeft = vis.contextContainer.left.selectAll('path').data(stackLeft);
+    // let pathRight = vis.contextContainer.right.selectAll('path').data(stackRight);
+
+    // let pathLeftEnter = pathLeft.enter()
+    //   .append('path')
+    //   .merge(pathLeft)
+    //     .attr('fill', d => colorScale(d.key))
+    //     .attr('d', areaLeft);
+
+    // let pathRightEnter = pathRight.enter()
+    //   .append('path')
+    //   .merge(pathRight)
+    //     .attr('fill', d => colorScale(d.key))
+    //     .attr('d', areaRight);
+
+  }
   renderSelector() {
     let vis = this;
 
-    let yRange = [
-      vis.config.margin.top + vis.config.contextHeight,
-      vis.config.margin.top + vis.config.contextHeight + vis.config.focusHeight
+    let xRange = [
+      vis.config.margin.left + vis.config.contextWidth,
+      vis.config.margin.left + vis.config.contextWidth + vis.config.focusWidth
     ]
-    let yScale = d3.scaleLinear().range([0, vis.config.focusHeight]);
-    let selectorHeight = yScale(vis.numFocused / vis.numConv);
+    let xScale = d3.scaleLinear().range([0, vis.config.focusWidth]);
+    let selectorWidth = xScale(vis.numFocused / vis.numConv);
 
     const drag = () => {
-      let dy = yRange[0] + (vis.config.focusHeight - selectorHeight / 2);
-      let selectRange = [selectorHeight - vis.config.focusHeight, 0];
-      let yValue = d3.event.y - dy;
-      if (yValue < selectRange[0]) {
-        yValue = selectRange[0];
-      } else if (yValue > selectRange[1]) {
-        yValue = selectRange[1];
+      let dx = xRange[0] + (vis.config.focusWidth - selectorWidth / 2);
+      let selectRange = [selectorWidth - vis.config.focusWidth, 0];
+      let xValue = d3.event.x - dx;
+      if (xValue < selectRange[0]) {
+        xValue = selectRange[0];
+      } else if (xValue > selectRange[1]) {
+        xValue = selectRange[1];
       }
 
-      d3.select('.scroll-selector').attr('y', yValue);
+      d3.select('.scroll-selector').attr('x', xValue);
 
       let dataScale = d3.scaleLinear()
         .domain([selectRange[0], selectRange[1]])
         .range([vis.numFocused, vis.numConv]);
 
-      let endIndex = Math.round(dataScale(yValue));
-      vis.focusIndex = {'start': endIndex - vis.numFocused, 'end': endIndex};
-      vis.renderFocus();
-      vis.renderContext();
-    }
 
-    const end = () => {
-      console.log("drag ended");
+      let endIndex = Math.round(dataScale(xValue));
+      let startIndex = endIndex - vis.numFocused;
+
+      if (startIndex != vis.focusIndex.start && endIndex != vis.focusIndex.end){
+        vis.focusIndex = {'start': endIndex - vis.numFocused, 'end': endIndex};
+        vis.renderFocus();
+        vis.renderTopicFocus();
+        vis.renderContext();
+        vis.renderTopicContext();
+      }
       
     }
 
+
     let selector = vis.svgContainer.append('rect')
-      .attr('transform', `translate(${vis.config.barWidth}, ${yRange[1] - selectorHeight})`)
+      .attr('transform', `translate(${xRange[1] - selectorWidth}, ${vis.config.barHeight + vis.config.heatMapHeight})`)
       .attr('class', 'scroll-selector')
-      .attr('height', selectorHeight)
-      .attr('width', 30)
+      .attr('width', selectorWidth)
+      .attr('height', 30)
       .attr('pointer-events', 'all') // Any part of the element
       .attr('cursor', 'ns-resize') // Bidirectional resize cursor
       .call(d3.drag()
--- js/index.js
+++ js/index.js
@@ -11,28 +11,42 @@ Promise.all([
     })
   })
 
-  let analysis = new ConvAnalysis({ parentElement: "#main" })
+  let numTopics = 25;
+  let numFocused = 50;
+
+  let analysis = new ConvAnalysis({
+    parentElement: "#main",
+    numTopics: numTopics,
+    numFocused: numFocused
+  })
 
   let sentiments = [[], [], [], [], []];
   let prev = 0;
 
+
   // Sort by datetime ascending
   conversations.sort((a, b) => Date.parse(a.date) - Date.parse(b.date))
   conversations.forEach((d, i) => {
-    d.sentWidth = Array(5).fill(0);
-    d.sentTotal = 0;
+
+    d.sentHeight = Array(5).fill(0); // The start and end y-value of each sentiment sub-bar
+    d.topicHeatmap = Array(numTopics).fill(0); // The topical heatmap grids that activates
+    d.sentTotal = 0; // Total number of messages in the conversation
     d.sent.forEach(s => {
-      d.sentWidth[analysis.sentimentBin(s.linePolarity)]++;
+      d.sentHeight[analysis.sentimentBin(s.linePolarity)]++;
       d.sentTotal++;
     })
     prev = 0;
-    d.sentWidth.forEach((width, j) => {
+    d.sentHeight.forEach((width, j) => {
       curr = prev + width / d.sentTotal;
       sentiments[j].push([prev, curr]);
       prev = curr;
     })
+    d.systemtopicid.forEach((topicIndex, j) => {
+      d.topicHeatmap[topicIndex] = 1;
+    })
   })
-  analysis.focusData = conversations;
-  analysis.contextData = sentiments;
+
+  // Assign values and update the visualization
+  analysis.data = conversations;
   analysis.updateVis();
 })
